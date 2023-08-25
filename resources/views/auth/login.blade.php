5b74e43d1ab00eda3748b51303e8380fa7efbeef
Fabian Esteban <fabi30bongo@gmail.com>
2023-08-25 13:31:16 -0600

    Corregido Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Inicio de Sesion') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login)') }}">
+                    <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
