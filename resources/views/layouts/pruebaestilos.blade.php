f68320ef5e614ecdc51a8c9c6b82f18519a55a16
Fabian Esteban <fabi30bongo@gmail.com>
2023-08-26 21:50:31 -0600

    CAMBIOS

---
+++ resources/views/layouts/pruebaestilos.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+</head>
+<body>
+    @yield('content')
+</body>
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+</html>
