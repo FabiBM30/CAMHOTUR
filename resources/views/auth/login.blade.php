c85175ef62efcaa9c1a7e1776e9ae4b8f3b82c10
Fabian Esteban <fabi30bongo@gmail.com>
2023-09-05 12:37:50 -0600

    correccion de login y registrar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
+{{-- @extends('pruebaestilos') --}}
 
-@section('contenido')
+@section('content')
 <div class="container mt-5">
 
     <div class="row justify-content-center">
